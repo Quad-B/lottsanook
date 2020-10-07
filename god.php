0dbf1aa55f50afbc9dea68ec5b8cd6f66627d7a5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-07 20:38:28 +0700

    wow

--- god.php
+++ god.php
@@ -11,8 +11,8 @@ $dom->preserveWhiteSpace = false;
 $bigel = $dom->getElementsByTagName('font');
 
 foreach($bigel as $val){
-    if(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล')){
+    //if(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล')){
         echo $val ->nodeValue.'<br>';
-    }
+    //}
 }
 ?>
