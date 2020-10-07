51da980144c5a68b0bd57f09d554840e4125f5b8
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-10-07 11:21:13 +0700

    Update findnum.php

--- findnum.php
+++ findnum.php
@@ -119,11 +119,13 @@ while($whatdate !== strtotime(date("d-m-Y"))) {
     { 
         foreach ($value as $inside)
         { 
-            //if($inside==0){
-            //    continue;
-            //} else {
-                echo $inside.'<br>';
-            //}
+            if($inside==;0;){
+                continue;
+            } else {
+                if($_GET['number'] == $inside){
+                    echo $inside.'<br>';
+                }
+            }
         }
     }
 
