3a8d675f9968e8ed76878d671cea1ea38ffbe46c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-07 22:24:44 +0700

    www

--- god.php
+++ god.php
@@ -18,15 +18,30 @@ while($year <= 2543) {
 
     foreach($bigel as $val){
         if(is_numeric(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล'))){
-            echo substr($val ->nodeValue, 74).'     ';
+            //echo substr($val ->nodeValue, 74).'     ';
             //echo $val ->nodeValue.'<br>';
-            //array_push($yearlist,"blue");
             $day = explode(" ", substr($val ->nodeValue, 74));
             //print_r($day);
             echo $day[0];
             echo $day[2];
             echo $day[3];
             echo '<br>';
+            switch ($day[2])
+    {
+      case 'มกราคม' : $monthnum="01"; break;
+      case 'กุมภาพันธ์' : $monthnum="02"; break;
+      case 'มีนาคม' : $monthnum="03"; break;
+      case 'เมษายน' : $monthnum="04"; break;
+      case 'พฤษภาคม' : $monthnum="05"; break;
+      case 'มิถุนายน' : $monthnum="06"; break;
+      case 'กรกฎาคม' : $monthnum="07"; break;
+      case 'สิงหาคม' : $monthnum="08"; break;
+      case 'กันยายน' : $monthnum="09"; break;
+      case 'ตุลาคม' : $monthnum="10"; break;
+      case 'พฤศจิกายน' : $monthnum="11"; break;
+      case 'ธันวาคม' : $monthnum="12"; break;
+    }
+            array_push($yearlist,sprintf("%08d",$day[0]).$monthnum.$day[3]);
         }
     }
 
