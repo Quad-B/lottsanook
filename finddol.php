cdfaacd53881d4fd34d70cde1d5d93386daa5a4d
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-10-07 13:11:58 +0700

    Update finddol.php

--- finddol.php
+++ finddol.php
@@ -17,25 +17,14 @@ while($whatdate !== strtotime(date("d-m-Y"))) {
         $aday = date("dm",$whatdate).date("Y",$whatdate)+543;
 
 $string  = file_get_contents('https://news.sanook.com/lotto/check/'.$aday.'/');
-$dom = new DOMDocument();
-//$dom->loadHTML(mb_convert_encoding($string, 'HTML-ENTITIES', 'UTF-8'));
-$dom->loadHTML($string);
-$dom->preserveWhiteSpace = false;
-//get all meta tags
-$el = $dom->getElementsByTagName('span');
 
-foreach($el as $val){
-
-        if ($val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 200,000 บาท' || $val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 100,000 บาท') {
+        if (strpos($string, 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 200,000 บาท') || strpos($string, 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 100,000 บาท')) {
             echo 'วันนี้หวยออก';
             $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-  fwrite($myfile, $aday);
-  fclose($myfile);
+            fwrite($myfile, $aday);
+            fclose($myfile);
         }
-        
-        break;
-
-}
+  
         $whatdate = strtotime("1 day", $whatdate);
 }
 ?>
