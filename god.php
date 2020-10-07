c02d3dfcda018d2e229da2cd3be3d8ecedb0e2bc
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-07 20:32:36 +0700

    wow

--- god.php
+++ god.php
@@ -2,8 +2,15 @@
 
 header('Access-Control-Allow-Origin: *');
 
-echo htmlspecialchars(file_get_contents("https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-2533.aspx"));
+//echo htmlspecialchars(file_get_contents("https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-2533.aspx"));
 
-//$string  = file_get_contents('https://news.sanook.com/lotto/check/'.$_GET['date'].'/');
+$string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-2533.aspx');
+$dom = new DOMDocument();
+$dom->loadHTML($string);
+$dom->preserveWhiteSpace = false;
+$bigel = $dom->getElementsByTagName('font');
 
+foreach($bigel as $val){
+    echo $val ->nodeValue;
+}
 ?>
