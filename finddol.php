a88d1bb4800ed5e49442083ce9ff33062069df8b
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-10-07 14:58:24 +0700

    Update finddol.php

--- finddol.php
+++ finddol.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 
 //echo htmlspecialchars(file_get_contents("https://news.sanook.com/lotto/check/01092560/"));
 
-$texttime = "29-12-2006";
+$texttime = "01-01-2020";
 
 $your_date = strtotime("1 day", strtotime($texttime));
 $new_date = date("dmY", $your_date);
