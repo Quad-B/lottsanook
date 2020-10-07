d495f421f13ee130ce65d645dd0df1ad28adbdf2
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-10-07 13:19:55 +0700

    Update finddol.php

--- finddol.php
+++ finddol.php
@@ -1,6 +1,5 @@
 <?php
 
-
 header('Access-Control-Allow-Origin: *');
 
 //echo htmlspecialchars(file_get_contents("https://news.sanook.com/lotto/check/01092560/"));
@@ -13,13 +12,13 @@ $new_date = date("dmY", $your_date);
 $whatdate = $your_date;
 while($whatdate <= strtotime(date("d-m-Y"))) {
         
-    $aday = date("dm",$whatdate).date("Y",$whatdate)+543;
-    if (strlen($aday) == 7) {
-        $aday = sprintf("%08d",date("dm",$whatdate).date("Y",$whatdate)+543);
-    }
+        $aday = date("dm",$whatdate).date("Y",$whatdate)+543;
+        if (strlen($aday) == 7) {
+                $aday = sprintf("%08d",date("dm",$whatdate).date("Y",$whatdate)+543);
+        }
         
 
-$string  = file_get_contents('https://news.sanook.com/lotto/check/'.$aday.'/');
+        $string  = file_get_contents('https://news.sanook.com/lotto/check/'.$aday.'/');
 
         if (strpos($string, 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 200,000 บาท') || strpos($string, 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 100,000 บาท')) {
             echo 'วันนี้หวยออก';
