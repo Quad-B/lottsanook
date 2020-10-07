d18b5cab108ca3c77714389275a412cc63426467
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-07 22:35:32 +0700

    www

--- god.php
+++ god.php
@@ -1,32 +1,16 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
-
 $year = 2533;
-
 $yearlist = array();
-
 while($year <= 2543) {
-
-    //echo htmlspecialchars(file_get_contents("https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-2533.aspx"));
-
     $string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'.strval($year).'.aspx');
     $dom = new DOMDocument();
     $dom->loadHTML($string);
     $dom->preserveWhiteSpace = false;
     $bigel = $dom->getElementsByTagName('font');
-
     foreach($bigel as $val){
         if(is_numeric(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล'))){
-            //echo substr($val ->nodeValue, 74).'     ';
-            //echo $val ->nodeValue.'<br>';
             $day = explode(" ", substr($val ->nodeValue, 74));
-            //print_r($day);
-            //echo $day[0];
-            //echo $day[2];
-            //echo $day[3];
-            //echo '<br>';
-
             switch ($day[2]){
                 case 'มกราคม' : $monthnum="01"; break;
                 case 'กุมภาพันธ์' : $monthnum="02"; break;
@@ -41,15 +25,10 @@ while($year <= 2543) {
                 case 'พฤศจิกายน' : $monthnum="11"; break;
                 case 'ธันวาคม' : $monthnum="12"; break;
             }
-
             array_push($yearlist,sprintf("%02d",$day[0]).$monthnum.$day[3]);
         }
     }
-
     $year += 1;
-
 }
-
 var_dump($yearlist);
-
 ?>
