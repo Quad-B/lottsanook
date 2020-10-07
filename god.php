23c7d68ae23f853428b9ff336e3fdff172375f8c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-07 22:57:11 +0700

    www

--- god.php
+++ god.php
@@ -4,7 +4,10 @@ $json_string  = file_get_contents('https://lottsanook.herokuapp.com/test.txt');
 $json_array  = json_decode($json_string, true);
 $elementCount  = count($json_array);
 $year = $json_array[$elementCount-1];
-$nextyear = $year+8;
+if($elementCount == 0){
+    $year = 2533;
+}
+$nextyear = $year+7;
 $yearlist = array();
 while($year <= $nextyear) {
     $string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'.strval($year).'.aspx');
