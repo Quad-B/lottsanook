b2fe44f136c0b1514f427d43a106b928ee260e55
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-07 21:00:41 +0700

    www

--- god.php
+++ god.php
@@ -18,7 +18,7 @@ while($year <= 2543) {
 
     foreach($bigel as $val){
         if(is_numeric(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล'))){
-            echo substr($val ->nodeValue, 50);
+            echo substr($val ->nodeValue, 60);
             //echo $val ->nodeValue.'<br>';
             //array_push($yearlist,"blue");
         }
