b8b9986862927f59d42a13c8b616bddcb64ea4e0
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-10-07 14:28:28 +0700

    Update finddol.php

--- finddol.php
+++ finddol.php
@@ -9,6 +9,7 @@ $texttime = "15-09-2020";
 $your_date = strtotime("1 day", strtotime($texttime));
 $new_date = date("dmY", $your_date);
 
+$wtf = '';
 $whatdate = $your_date;
 while($whatdate <= strtotime(date("d-m-Y"))) {
         
@@ -17,19 +18,16 @@ while($whatdate <= strtotime(date("d-m-Y"))) {
                 $aday = sprintf("%08d",date("dm",$whatdate).date("Y",$whatdate)+543);
         }
         
-
         $string  = file_get_contents('https://news.sanook.com/lotto/check/'.$aday.'/');
 
         if (strpos($string, 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 200,000 บาท') || strpos($string, 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 100,000 บาท')) {
-            echo 'วันนี้หวยออก';
-            $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-            fwrite($myfile, $aday);
-            fclose($myfile);
+            $wtf .= $aday
         }
   
-        echo $aday;
         $whatdate = strtotime("1 day", $whatdate);
 }
 
+$start  = file_get_contents('http://lottsanook.herokuapp.com/ohmymod.php?wow='.$aday.'/');
+$end = $start;
 echo 'เสร็จแล้ว';
 ?>
