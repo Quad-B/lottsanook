e3dc3f83c939207f1038dfb3eeb623cefabd028a
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 11:04:01 +0700

    www

--- god.php
+++ god.php
@@ -81,11 +81,41 @@ while($year <= $nextyear) {
     
     curl_multi_close($mh);
 
-    $response   =    [];
+    $response = [];
     for ($i=0; $i < 10; $i++) {
         $res    = curl_multi_getcontent($channels[$i]);
 
         $response[$i]  =   ($res === false) ? null : json_decode($res, true);
+
+        $string = $response[$i];
+        $dom = new DOMDocument();
+        $dom->loadHTML($string);
+        $dom->preserveWhiteSpace = false;
+        $bigel = $dom->getElementsByTagName('font');
+        foreach($bigel as $val){
+            if(is_numeric(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล'))){
+                $day = explode(" ", substr($val ->nodeValue, 74));
+                switch ($day[2]){
+                    case 'มกราคม' : $monthnum="01"; break;
+                    case 'กุมภาพันธ์' : $monthnum="02"; break;
+                    case 'มีนาคม' : $monthnum="03"; break;
+                    case 'เมษายน' : $monthnum="04"; break;
+                    case 'พฤษภาคม' : $monthnum="05"; break;
+                    case 'มิถุนายน' : $monthnum="06"; break;
+                    case 'กรกฎาคม' : $monthnum="07"; break;
+                    case 'สิงหาคม' : $monthnum="08"; break;
+                    case 'กันยายน' : $monthnum="09"; break;
+                    case 'ตุลาคม' : $monthnum="10"; break;
+                    case 'พฤศจิกายน' : $monthnum="11"; break;
+                    case 'ธันวาคม' : $monthnum="12"; break;
+                }
+                array_unshift($peryear,sprintf("%02d",$day[0]).$monthnum.$day[3]);
+            }
+        }
+
+        foreach($peryear as $val){
+            array_push($yearlist,$val);
+        }
     }
 
     //echo htmlspecialchars(print_r($response));
