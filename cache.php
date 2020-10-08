5bc15f6870d5e6c45ce2f4dc6e96621cf571b389
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 07:43:42 +0700

    test

--- cache.php
+++ cache.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 $year = 2533;
 $cachear = array();
-while($year-543 == date('Y')) {
+while($year-543 <= date('Y')) {
     $string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'.strval($year).'.aspx');
     array_push($cachear,$string);
     $year += 1;
