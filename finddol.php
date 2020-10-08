1752e3610505de2c9f80ac410ac8e334861ecbab
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 13:09:54 +0700

    test

--- finddol.php
+++ finddol.php
@@ -13,19 +13,13 @@ echo $json_array[0];
 
 foreach($json_array as $val){
 
-    echo $val;
-
     if($count <= 408){
         continue;
         $count += 1;
     }
 
-    echo $val;
-
     $string  = file_get_contents('https://lottsanook.herokuapp.com/?date='.$val.'');
     $number_array  = json_decode($string, true);
-
-    echo $string;
 
     if(array_search($_GET['search'], $number_array)){
         echo 'yes';
