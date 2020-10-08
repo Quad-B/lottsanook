58d248026fd413dfd50a88028c22b9cf0bbea0b6
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 13:21:34 +0700

    www

--- finddol.php
+++ finddol.php
@@ -1,29 +1,18 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
-
-//echo htmlspecialchars(file_get_contents("https://news.sanook.com/lotto/check/01092560/"));
-
 $json_string  = file_get_contents('https://lottsanook.herokuapp.com/test.txt');
 $json_array  = json_decode($json_string, true);
-
 $count = 0;
-
 foreach($json_array as $val){
-
     if($count <= 408){
         $count += 1;
         continue;
     }
-
     $string  = file_get_contents('https://lottsanook.herokuapp.com/?date='.$val.'');
     $number_array  = json_decode($string, true);
-    
     if(array_search($_GET['search'], $number_array)){
         echo $val;
     }
-
 }
-
 echo 'เสร็จแล้ว';
 ?>
