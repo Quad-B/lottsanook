ffa777bd50fa392026cdeef4199c68f5fd242f2f
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 07:41:37 +0700

    test

--- cache.php
+++ cache.php
@@ -4,7 +4,7 @@ $year = 2533;
 $cachear = array();
 while($year-543 == date('Y')) {
     $string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'.strval($year).'.aspx');
-    array_push($cachear,$string)
+    array_push($cachear,$string);
     $year += 1;
 }
 $file = fopen("cache.txt","w");
