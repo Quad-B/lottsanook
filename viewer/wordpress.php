4c6641c053a8e9d90cc6d2c37e971bb0de0748d8
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-10-08 22:23:10 +0700

    Create wordpress.php

---
+++ viewer/wordpress.php
@@ -0,0 +1,19 @@
+<?php
+$rss = new DOMDocument();
+$rss->load('https://boyphongsakorn.wordpress.com/feed/');
+
+$feed = array();
+foreach ($rss->getElementsByTagName('item') as $node)
+{
+    $item = array ( 
+    'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+    'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+    'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+    'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+    'img' => $node->getElementsByTagName('content')->item(0)->getAttribute("url"),
+    );
+array_push($feed, $item);
+}
+
+echo json_encode($feed);
+?>
