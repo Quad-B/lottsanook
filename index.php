065fa2331d0548905d889af49f4ff739edd54e57
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 23:04:48 +0700

    test

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($ch);
 curl_close($ch);
+echo $response.'<br>';
 $dom = new DOMDocument();
 $dom->loadHTML($response);
 $dom->preserveWhiteSpace = false;
