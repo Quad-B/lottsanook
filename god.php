584532f47ee77443e0efd383778c66d64a649184
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-08 08:34:45 +0700

    test

--- god.php
+++ god.php
@@ -65,43 +65,43 @@ while($year <= $nextyear) {
     do {
         $status = curl_multi_exec($mh, $active);
         if ($active) {
-            //curl_multi_select($mh);
+            for ($i=1; $i <= 10; $i++) { 
+                //$var = "ch$i";
+                $string = curl_multi_select($mh);
+                //($$var)
+                $peryear = array();
+                //$string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'.strval($year).'.aspx');
+                $dom = new DOMDocument();
+                $dom->loadHTML($string);
+                $dom->preserveWhiteSpace = false;
+                $bigel = $dom->getElementsByTagName('font');
+                foreach($bigel as $val){
+                    if(is_numeric(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล'))){
+                        $day = explode(" ", substr($val ->nodeValue, 74));
+                        switch ($day[2]){
+                            case 'มกราคม' : $monthnum="01"; break;
+                            case 'กุมภาพันธ์' : $monthnum="02"; break;
+                            case 'มีนาคม' : $monthnum="03"; break;
+                            case 'เมษายน' : $monthnum="04"; break;
+                            case 'พฤษภาคม' : $monthnum="05"; break;
+                            case 'มิถุนายน' : $monthnum="06"; break;
+                            case 'กรกฎาคม' : $monthnum="07"; break;
+                            case 'สิงหาคม' : $monthnum="08"; break;
+                            case 'กันยายน' : $monthnum="09"; break;
+                            case 'ตุลาคม' : $monthnum="10"; break;
+                            case 'พฤศจิกายน' : $monthnum="11"; break;
+                            case 'ธันวาคม' : $monthnum="12"; break;
+                        }
+                        array_unshift($peryear,sprintf("%02d",$day[0]).$monthnum.$day[3]);
+                    }
+                }
+                foreach($peryear as $val){
+                    array_push($yearlist,$val);
+                }
+            }
         }
     } while ($active && $status == CURLM_OK);
 
-    for ($i=1; $i <= 10; $i++) { 
-        $var = "ch$i";
-        $string = curl_multi_getcontent($$var);
-        $peryear = array();
-        //$string  = file_get_contents('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'.strval($year).'.aspx');
-        $dom = new DOMDocument();
-        $dom->loadHTML($string);
-        $dom->preserveWhiteSpace = false;
-        $bigel = $dom->getElementsByTagName('font');
-        foreach($bigel as $val){
-            if(is_numeric(strpos($val ->nodeValue, 'ตรวจสลากกินแบ่งรัฐบาล'))){
-                $day = explode(" ", substr($val ->nodeValue, 74));
-                switch ($day[2]){
-                    case 'มกราคม' : $monthnum="01"; break;
-                    case 'กุมภาพันธ์' : $monthnum="02"; break;
-                    case 'มีนาคม' : $monthnum="03"; break;
-                    case 'เมษายน' : $monthnum="04"; break;
-                    case 'พฤษภาคม' : $monthnum="05"; break;
-                    case 'มิถุนายน' : $monthnum="06"; break;
-                    case 'กรกฎาคม' : $monthnum="07"; break;
-                    case 'สิงหาคม' : $monthnum="08"; break;
-                    case 'กันยายน' : $monthnum="09"; break;
-                    case 'ตุลาคม' : $monthnum="10"; break;
-                    case 'พฤศจิกายน' : $monthnum="11"; break;
-                    case 'ธันวาคม' : $monthnum="12"; break;
-                }
-                array_unshift($peryear,sprintf("%02d",$day[0]).$monthnum.$day[3]);
-            }
-        }
-        foreach($peryear as $val){
-            array_push($yearlist,$val);
-        }
-    }
     $year += 10;
 }
 $file = fopen("test.txt","w");
