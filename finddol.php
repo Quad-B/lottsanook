1020bf165a9a567daf4bbf06d4a483446a2dc3ed
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-13 22:34:37 +0700

    test

--- finddol.php
+++ finddol.php
@@ -8,7 +8,7 @@ $count = 0;
 $allwin = array();
 $mh = curl_multi_init();
 $channels = [];
-foreach($json_array as $val){
+/*foreach($json_array as $val){
     if($count <= 408){
         $count += 1;
         continue;
@@ -20,19 +20,48 @@ foreach($json_array as $val){
     $response = curl_exec($ch);
     curl_close($ch);
     $number_array  = json_decode($response);
-    //var_dump($number_array);
     foreach($number_array as $vall){
         if (in_array($_GET['search'], $vall))
         {
             array_push($allwin,$number_array[0][0]);
-            //echo $number_array[0][0];
+        }
+    }
+}*/
+
+foreach($json_array as $id){
+    $fetchURL = "https://lottsanook.herokuapp.com/?date=".$id."&from";
+    
+    $channels[$id] = curl_init($fetchURL);
+    curl_setopt($channels[$id], CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($channels[$id], CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($channels[$id], CURLOPT_SSL_VERIFYPEER, 0);
+    curl_multi_add_handle($mh, $channels[$id]);
+}
+
+$running = null;
+do {
+    curl_multi_exec($mh, $running);
+    curl_multi_select($mh);
+} while ($running > 0);
+
+foreach ($json_array as $id) {
+    curl_multi_remove_handle($mh, $channels[$id]);
+}
+
+curl_multi_close($mh);
+
+foreach($json_array as $id){
+    $res    = curl_multi_getcontent($channels[$id]);
+
+    $number_array  = json_decode($res);
+    foreach($number_array as $vall){
+        if (in_array($_GET['search'], $vall))
+        {
+            array_push($allwin,$number_array[0][0]);
         }
     }
 }
 
-/*foreach($json_array as $val){
-}*/
 
-//echo $_GET['search'];
 echo json_encode($allwin);
 ?>
