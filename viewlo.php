01c0acb7e4ff5592b6c7ddbbc765f4ed7c13ed02
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-14 02:31:34 +0700

    test

--- viewlo.php
+++ viewlo.php
@@ -4,8 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
 </head>
 <body>
-    ทดสอบๆ
+    ผลการออกสลากกินแบ่งรัฐบาล ประจำวันที่ 1 ตุลาคม 2563
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        รางวัลที่1
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">000000</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        รางวัลที่1
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">000000</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        รางวัลที่1
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">000000</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        รางวัลที่1
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">000000</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
 </body>
 </html>
