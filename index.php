cac7c0274d455c1c8dfffa8c2774c5a2e1bb804c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-13 19:30:36 +0700

    change to read first

--- index.php
+++ index.php
@@ -4,26 +4,6 @@ $filename = $_GET['date'].".txt";
 $day = substr($_GET['date'], 0,2);
 $month = substr($_GET['date'], 2,2);
 $year = substr($_GET['date'], 4,4);
-if ($year == date('Y')+543) {
-    if (isset($_GET['from'])) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date'].'&from');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        echo $response;
-        //header('Location: https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date'].'&from');
-    }else{
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date']);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        echo $response;
-        //header('Location: https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date']);
-    }
-    exit();
-}
 switch ($month)
     {
       case '01' : $monthtext="มกราคม"; break;
@@ -49,6 +29,26 @@ if(file_exists($filename)){
     }
     echo $readwow;
     fclose($myfile);
+    exit();
+}
+if ($year == date('Y')+543) {
+    if (isset($_GET['from'])) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date'].'&from');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        echo $response;
+        //header('Location: https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date'].'&from');
+    }else{
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        echo $response;
+        //header('Location: https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date']);
+    }
     exit();
 }
 $url = "https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%87%E0%B8%A7%E0%B8%94-".$day."-".urlencode($monthtext)."-".$year.".aspx";
