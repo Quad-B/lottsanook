196c5fbf1a7640864f93d35a76d44a96668cea49
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-10-15 09:08:40 +0700

    test

--- viewlo.php
+++ viewlo.php
@@ -114,6 +114,13 @@
         }
         ?>
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+    <script>
+    $.getJSON('https://lottsanook.herokuapp.com/finddol.php?search=<?php echo $numsel ?>', function(data) {
+        console.log(data.length)
+    });
+    </script>
 </body>
 </html>
