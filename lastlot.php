76470b5293876c162b39ec69cf61da9a8dda01c0
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-11-16 15:28:00 +0700

    test

--- lastlot.php
+++ lastlot.php
@@ -1,3 +1,5 @@
 <?php
-echo json_encode(array("win"=>"XC90","threefirst"=>"X5","threeend"=>"Highlander","twoend"=>"Highlander"));
+$days=json_decode(file_get_contents("https://lottsanook.herokuapp.com/god.php"));
+$get=json_decode(file_get_contents("https://lottsanook.herokuapp.com/?date=".end($days)));
+echo json_encode(array("win"=>$get[0][1],"threefirst"=>"X5","threeend"=>"Highlander","twoend"=>"Highlander"));
 ?>
