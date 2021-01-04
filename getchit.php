7ef609251a50b8ae743ba045b615f2507759d9b5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-01-05 04:16:27 +0700

    test

---
+++ getchit.php
@@ -0,0 +1,15 @@
+<?php
+$url = "https://www.lottovip.co/%E0%B8%AB%E0%B8%A7%E0%B8%A2%E0%B9%84%E0%B8%97%E0%B8%A2%E0%B8%A3%E0%B8%B1%E0%B8%90/";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+curl_close($ch);
+$dom = new DOMDocument();
+$dom->loadHTML($response);
+$dom->preserveWhiteSpace = false;
+$el = $dom->getElementsByTagName('img');
+foreach($el as $val){
+    echo $val ->nodeValue;
+}
+?>
