108eaf0517429da2b9a8f173df0ace34a44ae51f
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-01-25 03:02:06 +0700

    test

--- getcolor.php
+++
@@ -1,9 +0,0 @@
-<?php
-$im = imagecreatefrompng("https://image.joox.com/JOOXcover/0/6e93d6e3a4f7ca69/640");
-$rgb = imagecolorat($im, "500", "500");
-$r = ($rgb >> 16) & 0xFF;
-$g = ($rgb >> 8) & 0xFF;
-$b = $rgb & 0xFF;
-
-var_dump($r, $g, $b);
-?>
