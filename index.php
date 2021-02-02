8ea9281f9ec37bc09e78ad0527ce967f446f66e8
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-02-02 23:25:33 +0700

    test

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if(file_exists("cache/".$filename)){
     fclose($myfile);
     exit();
 }
-if ($year == date('Y')+542) {
+if ($year == date('Y')+543) {
     if (isset($_GET['from'])) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date'].'&from');
