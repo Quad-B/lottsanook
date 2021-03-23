1615306eaf33f8186d7e083a6c839a90b64d3ef5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-03-23 08:45:30 +0700

    test

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if(file_exists("cache/".$filename)){
     fclose($myfile);
     exit();
 }
-if ($year == date('Y')+543) {
+/*if ($year == date('Y')+543) {
     if (isset($_GET['from'])) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://lottsanook.herokuapp.com/index2.php?date='.$_GET['date'].'&from');
@@ -53,7 +53,7 @@ if ($year == date('Y')+543) {
         echo $response;
     }
     exit();
-}
+}*/
 $url = "https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%87%E0%B8%A7%E0%B8%94-".$day."-".urlencode($monthtext)."-".$year.".aspx";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
