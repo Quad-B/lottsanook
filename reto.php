e268c7c3a562450ee4f8c490e3b6c43ed7e8ca62
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-04-10 22:48:48 +0700

    test

---
+++ reto.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$url = "https://lottsanook.herokuapp.com/?date=".date("dmY");
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+curl_close($ch);
+$result=json_decode($response);
+if($result[8][100] == "xxxxxx"){
+    echo "yes";
+} else {
+    echo "no";
+}
+?>
