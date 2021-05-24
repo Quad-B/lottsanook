ba5f0ef05f1bb8ea6b3a75be8af0551790c7c6ae
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-24 18:37:05 +0700

    wow

--- index3.php
+++ index3.php
@@ -71,67 +71,6 @@ if (isset($_GET['from'])) {
 }
 $minwave = 0;
 $maxwave = 5;
-/*foreach($el as $val){
-    if ($val -> getAttribute('class') == 'lotto__number' || $val -> getAttribute('class') == 'default-font--reward') {
-        if ($minwave >= 1 && $minwave <= $maxwave) {
-            $lottapi[$wave][$minwave] = $val ->nodeValue;
-            $minwave++;
-        } else {
-            $minwave = 0;
-        }
-        if ($val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 200,000 บาท' || $val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 2 มี 5 รางวัลๆละ 100,000 บาท') {
-            $minwave += 1;
-            $maxwave = 5;
-            $wave = 5;
-        }
-        if ($val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 3 มี 10 รางวัลๆละ 80,000 บาท' || $val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 3 มี 10 รางวัลๆละ 40,000 บาท') {
-            $minwave += 1;
-            $maxwave = 10;
-            $wave = 6;
-        }
-        if ($val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 4 มี 50 รางวัลๆละ 40,000 บาท' || $val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 4 มี 50 รางวัลๆละ 20,000 บาท') {
-            $minwave += 1;
-            $maxwave = 50;
-            $wave = 7;
-        }
-        if ($val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 5 มี 100 รางวัลๆละ 20,000 บาท' || $val ->nodeValue == 'ผลสลากกินแบ่งรัฐบาล รางวัลที่ 5 มี 100 รางวัลๆละ 10,000 บาท') {
-            $minwave += 1;
-            $maxwave = 100;
-            $wave = 8;
-        }
-    }
-    if ($val ->nodeValue == 'รางวัลที่ 1') {
-        $lottapi[0][1] = $bigel[0] ->nodeValue;
-    } else if ($val ->nodeValue == 'เลขหน้า 3 ตัว') {
-        $lottapi[1][1] = $bigel[1] ->nodeValue;
-        $lottapi[1][2] = $bigel[2] ->nodeValue;
-    } else if ($val ->nodeValue == 'เลขท้าย 3 ตัว') {
-        if (substr($_GET['date'],4,4) <= 2558) {
-            if (substr($_GET['date'],2,2) <= 8 && substr($_GET['date'],4,4) <= 2558 || substr($_GET['date'],4,4) < 2558) {
-                $lottapi[2][1] = $bigel[1] ->nodeValue;
-                $lottapi[2][2] = $bigel[2] ->nodeValue;
-                $lottapi[2][3] = $bigel[3] ->nodeValue;
-                $lottapi[2][4] = $bigel[4] ->nodeValue;
-            } else {
-                $lottapi[2][1] = $bigel[3] ->nodeValue;
-                $lottapi[2][2] = $bigel[4] ->nodeValue;
-            }
-        } else {
-            $lottapi[2][1] = $bigel[3] ->nodeValue;
-            $lottapi[2][2] = $bigel[4] ->nodeValue;
-        }
-    } else if ($val ->nodeValue == 'เลขท้าย 2 ตัว') {
-        $lottapi[3][1] = $bigel[5] ->nodeValue;
-    } else if ($val ->nodeValue == 'รางวัลข้างเคียงรางวัลที่ 1') {
-        if (is_numeric($bigel[6] ->nodeValue)) {
-            $lottapi[4][1] = $bigel[6] ->nodeValue;
-            $lottapi[4][2] = $bigel[7] ->nodeValue;
-        } else {
-            $lottapi[4][1] = $bigel[7] ->nodeValue;
-            $lottapi[4][2] = $bigel[8] ->nodeValue;
-        }
-    }
-}*/
 $arcount=0;
 foreach ($edog as $val) {
     //echo $val->getElementsByTagName('strong');
