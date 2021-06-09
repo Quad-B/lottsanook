291631220187ff3b339f3aeaad3d8898b115cec1
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-06-09 21:11:24 +0700

    test

--- lastlot.php
+++ lastlot.php
@@ -3,5 +3,9 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 $days=json_decode(file_get_contents("https://lottsanook.herokuapp.com/god.php"));
 $get=json_decode(file_get_contents("https://lottsanook.herokuapp.com/?date=".end($days)));
-echo json_encode(array("win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
+if(isset($_GET['info'])){
+    echo json_encode(array("win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
+}else{
+    echo json_encode(array("info"=>array("date"=>end($days)),"win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));55                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+}
 ?>
