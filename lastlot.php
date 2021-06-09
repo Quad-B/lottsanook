ca4e6522fce7db948205848164008e9d5f795aa5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-06-09 21:16:48 +0700

    test

--- lastlot.php
+++ lastlot.php
@@ -6,6 +6,6 @@ $get=json_decode(file_get_contents("https://lottsanook.herokuapp.com/?date=".end
 if(isset($_GET['info'])){
     echo json_encode(array("win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
 }else{
-    echo json_encode(array("info"=>array("date"=>end($days)),"win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));55                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    echo json_encode(array("info"=>array("date"=>end($days)),"win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
 }
 ?>
