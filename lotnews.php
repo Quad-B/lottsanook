36e3d851813d676ec82f33b924bfe752782f411a
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-08 18:01:37 +0700

    test

--- lotnews.php
+++ lotnews.php
@@ -51,21 +51,20 @@ $array = json_decode($json,TRUE);
 //loop news 5 times
 //print_r($array);
 for($i=0;$i<5;$i++){
-     $title = $array['channel']['item'][$i]['title'];
-     $link = $array['channel']['item'][$i]['link'];
-     $description = $array['channel']['item'][$i]['description'];
-     $pubDate = $array['channel']['item'][$i]['pubDate'];
-     //$image = $array['channel']['item'][$i]['enclosure']['@attributes']['url'];
-     /*echo $title;
-     echo $link;
-     echo $description;
-     echo $pubDate;
-     echo $image;*/
-     //cut description to 100 char and add ...
-     $description = strip_tags($array['channel']['item'][$i]['description']);
-     $a=array($title,$link,$description,$pubDate);
-     $a[2] = array_walk($description, "fcn");
-     array_push($cars,$a);
+    $title = $array['channel']['item'][$i]['title'];
+    $link = $array['channel']['item'][$i]['link'];
+    $description = strip_tags($array['channel']['item'][$i]['description']);
+    $pubDate = $array['channel']['item'][$i]['pubDate'];
+    //$image = $array['channel']['item'][$i]['enclosure']['@attributes']['url'];
+    /*echo $title;
+    echo $link;
+    echo $description;
+    echo $pubDate;
+    echo $image;*/
+    //cut description to 100 char and add ...
+    echo substr($description,0,100)."...";
+    $a=array($title,$link,$description,$pubDate);
+    array_push($cars,$a);
 }
 
 echo json_encode($cars);
