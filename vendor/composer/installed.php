a5865e77104409fa781e591ead5a1e42ed201adf
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-08 19:01:38 +0700

    ok

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'efebf8bd3ffd83ec6fab6e551b5ed571dbd4a85f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'efebf8bd3ffd83ec6fab6e551b5ed571dbd4a85f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
