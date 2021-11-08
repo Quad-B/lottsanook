4f50bcb02eb57dbe53568a26746e697b574b011c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-08 19:15:18 +0700

    test

--- lotnews.php
+++ lotnews.php
@@ -70,6 +70,9 @@ for($i=0;$i<5;$i++){
     $link = $array['channel']['item'][$i]['link'];
     $description = mb_substr(strip_tags($array['channel']['item'][$i]['description']),0,100,'UTF-8').'...';
     $pubDate = $array['channel']['item'][$i]['pubDate'];
+    $image = $rss->children("https://www.khaosod.co.th/wpapp/uploads/")->thumbnail[1]->attributes();
+    $image_link = $image['url'];
+    echo $image_link;
     //$image = $array['channel']['item'][$i]['enclosure']['@attributes']['url'];
     //cut description to 100 char and add ...
     $a=array($title,$link,$description,$pubDate);
