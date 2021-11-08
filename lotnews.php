8b33c608bd0ae79773d037819c1d798509152a92
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-08 16:20:37 +0700

    test

--- lotnews.php
+++ lotnews.php
@@ -66,7 +66,7 @@ for($i=0;$i<5;$i++){
 
 echo json_encode($cars);
 
-$curl = curl_init();
+/*$curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => 'https://www.khaosod.co.th/tag/เลขเด็ด/feed',
@@ -104,7 +104,7 @@ for($i=0;$i<5;$i++){
     echo $pubDate;
     echo $image;*/
     //cut description to 100 char and add ...
-    $description = substr($description,0,100);
+    /*$description = substr($description,0,100);
     $description = $description."...";
     $a=array($title,$link,$description,$pubDate);
     array_push($cars,$a);
@@ -147,12 +147,12 @@ for($i=0;$i<5;$i++){
     echo $pubDate;
     echo $image;*/
     //cut description to 100 char and add ...
-    $description = substr($description,0,100);
+    /*$description = substr($description,0,100);
     $description = $description."...";
     $a=array($title,$link,$description,$pubDate);
     array_push($cars,$a);
 }
 
-echo json_encode($cars);
+echo json_encode($cars);*/
 //print_r($json);
 ?>
