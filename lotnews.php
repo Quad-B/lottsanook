c1d242eb0fa06810c7d3ac9e542abc984210c89d
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-08 18:13:52 +0700

    test

--- lotnews.php
+++ lotnews.php
@@ -26,24 +26,6 @@ $response = curl_exec($curl);
 curl_close($curl);
 //echo $response;
 
-//$removed = preg_replace('/^\s*\/\/<!\[CDATA\[([\s\S]*)\/\/\]\]>\s*\z/', '$1', $response);
-
-//get from xml
-/*$xml = new SimpleXMLElement($response);
-//get title and link of each news
-print_r($xml->channel->item->description);
-foreach ($xml->channel->item as $item) {
-    preg_match("/^<!\[CDATA\[(.*)\]\]>$/s", $item->description, $out);
-  $cars[] = array(
-    'title' => $item->title,
-    'link' => $item->link,
-    'description' => $out[1],
-    'pubDate' => $item->pubDate,
-    'guid' => $item->guid,
-  );
-}*/
-//print_r(json_encode($cars));
-
 $xml = simplexml_load_string($response, 'SimpleXMLElement', LIBXML_NOCDATA);
 $json = json_encode($xml);
 $array = json_decode($json,TRUE);
@@ -61,11 +43,7 @@ for($i=0;$i<5;$i++){
     array_push($cars,$a);
 }
 
-$cars[0][2] = substr($cars[0][2],0,100)."...";
-
-echo json_encode($cars);
-
-/*$curl = curl_init();
+$curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => 'https://www.khaosod.co.th/tag/เลขเด็ด/feed',
@@ -97,17 +75,16 @@ for($i=0;$i<5;$i++){
     print_r($array['channel']['item'][$i]['description']);
     $pubDate = $array['channel']['item'][$i]['pubDate'];
     //$image = $array['channel']['item'][$i]['enclosure']['@attributes']['url'];
-    /*echo $title;
-    echo $link;
-    echo $description;
-    echo $pubDate;
-    echo $image;*/
     //cut description to 100 char and add ...
-    /*$description = substr($description,0,100);
-    $description = $description."...";
     $a=array($title,$link,$description,$pubDate);
     array_push($cars,$a);
 }
+
+foreach ($cars as $key => $value) {
+    $cars[$key][2] = substr($cars[$key][2],0,100)."...";
+}
+
+echo json_encode($cars);
 
 /*$curl = curl_init();
 
